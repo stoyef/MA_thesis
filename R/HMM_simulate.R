a9514075ac88911d7e9a5f1cd4313c8cba434d11
stoyef <62142204+stoyef@users.noreply.github.com>
2022-12-13 13:58:14 +0100

    sample_arp update (individual p_j)

--- R/HMM_simulate.R
+++ R/HMM_simulate.R
@@ -17,12 +17,13 @@
 #' @param N Number of states.
 #' @param params Parameter vector for the different distributions. 
 #'.              Has to respect the order specified in \code{dists}.
-#' @param autocor List of parameter matrix (Dimensions for each matrix: \eqn{n\times p}) for the autoregression parameters. 
+#' @param autocor List of lists of parameters for the autoregression parameters. 
 #'             Has to match p, in the order \eqn{\phi_{t-p},\dots,\phi_{t-1}}
 #'             where \eqn{\phi} is the vector of autoregression parameters
-#'             for one specific time lag (one value for each state).
-#'             0, if no autoregression Has to respect the order specified in \code{dists}.
-#' @param p Vector of degree of autoregression for each distribution, 0 = no autoregression
+#'             for one specific time lag (one value for each state, some values can be NA).
+#'             0, if no autoregression. Has to respect the order specified in \code{dists}.
+#' @param p Vector of degree of autoregression for each distribution, 0 = no autoregression.
+#'          Allows individual values for every state (one entry for every state of every variable)
 #' @param dists Vector containing abbreviated names (in R-jargon) of the distributions 
 #'              to be considered in the likelihood computation.
 #'               
@@ -77,19 +78,20 @@ sample_arp <- function(n_samples, delta, Gamma, N, params, autocor, p, dists){
     }
   } else{ # autocorrelation
     for (t in (max(p)+1):n_samples){
-      states[t] <- sample(1:N, 1, prob=Gamma[states[t-1],])
+      states[t] <- sample(1:N, 1, prob=Gamma[states[t-1],]) # current state
       for (dist in 1:length(dists)){
         param1=params[(2*(dist-1)*N)+1:N] # first parameter
         param2=params[(2*(dist-1)*N)+N+1:N] # second parameter
-        ar_matrix <- autocor[[dist]]
+        ar_dist <- autocor[[dist]]
         
+        ## only for gamma and von Mises distribution (and for normal distribution although this is not tested properly)
         if (dists[dist]=='vm'){ # compute mean value on a circle
-          param1_ar_auto <- sum(ar_matrix[states[t],]*exp(complex(imaginary=data[(t-p[dist]):(t-1),dist])))
-          param1_ar_fixed <- (1-sum(ar_matrix[states[t],]))*exp(complex(imaginary=param1[states[t]]))
+          param1_ar_auto <- sum(ar_dist[[states[t]]]*exp(complex(imaginary=data[(t-p[(dist-1)*N+states[t]]):(t-1),dist])))
+          param1_ar_fixed <- (1-sum(ar_dist[[states[t]]]))*exp(complex(imaginary=param1[states[t]]))
           param1_ar <- Arg(param1_ar_auto + param1_ar_fixed)
         } else{
-          param1_ar <- sum(ar_matrix[states[t],]*data[(t-p[dist]):(t-1),dist]) + 
-            (1-sum(ar_matrix[states[t],]))*param1[states[t]]
+          param1_ar <- sum(ar_dist[[states[t]]]*data[(t-p[(dist-1)*N+states[t]]):(t-1),dist]) + 
+            (1-sum(ar_dist[[states[t]]]))*param1[states[t]]
         }
         
         if (dists[dist]=='gamma'){ # respect ccv

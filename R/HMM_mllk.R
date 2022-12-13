638c4d4a44554f3dbbb2a99709ff79f91a81383e
stoyef <62142204+stoyef@users.noreply.github.com>
2022-12-13 14:02:48 +0100

    allprobs update (individual p_j)

--- R/HMM_mllk.R
+++ R/HMM_mllk.R
@@ -65,4 +65,78 @@ mllk <- function(theta.star, dists, x, N, p_auto, scale_kappa=1, zero_inf=FALSE)
 
 
 
+#' Matrix of all probabilities
+#' 
+#' Calculate matrix of all probabilities for an AR(p)-HMM with or without within-state autoregression.
+#' 
+#' @param x Data matrix the model was fitted to.
+#' @param dists Vector of distributions in R-jargon.
+#' @param autocor list of autoregression vectors, respecting order of dists, one vector for each state.
+#' @param params List of optimized parameters, returned by fitting the HMM.
+#' @param N Number of states.
+#' @param p Vector of degree of autoregression for each distribution (0 = no autoregression).
+#' 
+#' @return Matrix of all probabilities.
+#' 
+#' @export
+#' @rdname allprobs
+allprobs <- function(x, dists, autocor=0, params, N, p){
+  if (is.vector(x)) x <- matrix(x, nrow=length(x))
+  allprobs <- matrix(1,dim(x)[1],N)
+  
+  for (dist in 1:length(dists)){
+    if (any(p[((dist-1)*N) + 1:N]>0)){
+      
+      for (j in 1:N){ 
+        if (p[(dist-1)*N + j] > 0){ # check if current state has autoregression
+          
+          ind <- which(!is.na(x[,dist]))[-c(1:p[((dist-1)*N) + j])] 
+          autocor_ind <- matrix(NA,nrow=length(ind),ncol=p[((dist-1)*N) + j]) 
+          
+          for (i in 1:p[((dist-1)*N) + j]){
+            autocor_ind[,i] <- ind-p[((dist-1)*N) + j]+i-1
+          }
+          
+          x_wo_na = x
+          x_wo_na[which(is.na(x_wo_na[,dist])),dist] = mean(x_wo_na[,dist],na.rm = TRUE)
+          autocor_ind <- apply(autocor_ind, 2, function(a)x_wo_na[a,dist]) 
+          
+          theta_j <- params[[dist]]
+          for (i in names(theta_j)) theta_j[i][[1]] = theta_j[i][[1]][j] 
+          
+          allprobs[ind,j] <- allprobs[ind,j] * 
+            match.fun(paste('dens_', dists[dist], sep=""))(x[ind,dist], theta_j, 
+                                                           autocor_ind, 
+                                                           autocor[[dist]][[j]],
+                                                           p[((dist-1)*N) + j])
+        } else{ # current state has no autoregression
+          ind <- which(!is.na(x[,dist]))
+          for (j in 1:N){
+            
+            theta_j <- params[[dist]]
+            for (i in names(theta_j)) theta_j[i][[1]] = theta_j[i][[1]][j] 
+            
+            allprobs[ind,j] <- allprobs[ind,j] * 
+              match.fun(paste('dens_', dists[dist], sep=""))(x[ind,dist], theta_j)
+          }
+        }
+      }
+    } else{ 
+      
+      ind <- which(!is.na(x[,dist]))
+      
+      for (j in 1:N){
+        theta_j <- params[[dist]]
+        # theta_j consists the parameters of state j for each parameter in theta$params
+        for (i in names(theta_j)) theta_j[i][[1]] = theta_j[i][[1]][j] 
+        
+        allprobs[ind,j] <- allprobs[ind,j] * 
+          match.fun(paste('dens_', dists[dist], sep=""))(x[ind,dist], theta_j)
+      }
+    }
+  }
+  return(allprobs)
+}
+
+
 
